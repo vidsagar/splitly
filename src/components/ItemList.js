8a8af90e1c6becf197ca6fb47340ab0bea63c978
Victor <victor@Victors-MacBook-Pro.local>
2025-11-14 21:20:40 -0600

    dynamic users

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,43 +4,42 @@ import PortionManager from "./PortionManager";
 
 
 export const ItemList = () => {
-    const {items} = useReceipt();
-    const {focused} = useReceipt();
+	const { items } = useReceipt();
+	const { focused } = useReceipt();
 
-    const {onItemChange} = useReceipt();
-    const {onTaxClick} = useReceipt();
-    const {onUserClick} = useReceipt();
-    const {handleFocus} = useReceipt();
-    const {onDeleteClick} = useReceipt();
-    return (
-        <>
-            {items.map( 
-            (inputItem, index) => (
-                    <div 
-                    className = "item-input-form-row"
-                    key = {index}
-                    >
-                    <ItemRow 
-                        key={index}
-                        item = {inputItem}
-                        onFocus = {() => handleFocus(index)}
-                        onItemChange = {(e) => onItemChange(e, index)}
-                        onDeleteClick = {() => onDeleteClick(index)}
-                    />
-                    {
-                        (index===focused)?
-                        <PortionManager
-                            index = {index} 
-                            onTaxClick = {(e) => {return onTaxClick(e, index)}}
-                            onUserClick = {(e) => {return onUserClick(e, index)}}
-                            inputItem = {inputItem}
-                            users = {inputItem.users}
-                        />
-                        :<></>
-                    }
-                    </div>
-                )
-            )}
-        </>
-    )
+	const { onItemChange } = useReceipt();
+	const { onTaxClick } = useReceipt();
+	const { onItemUserClick } = useReceipt();
+	const { handleFocus } = useReceipt();
+	const { onDeleteClick } = useReceipt();
+	return (
+		<>
+			{items?.map(
+				(inputItem, index) => (
+					<div
+						className="item-input-form-row"
+						key={index}
+					>
+						<ItemRow
+							key={index}
+							item={inputItem}
+							onFocus={() => handleFocus(index)}
+							onItemChange={(e) => onItemChange(e, index)}
+							onDeleteClick={() => onDeleteClick(index)}
+						/>
+						{
+							(index === focused) &&
+							<PortionManager
+								index={index}
+								onTaxClick={(e) => onTaxClick(e, index)}
+								onItemUserClick={onItemUserClick}
+								inputItem={inputItem}
+								itemUsers={inputItem.users}
+							/>
+						}
+					</div>
+				)
+			)}
+		</>
+	)
 }
--- src/context/UserProvider.js
+++ src/context/UserProvider.js
@@ -2,12 +2,31 @@ import { UserContext } from "./UserContext";
 import { useContext, useState } from "react";
 
 export const UserProvider = ({ children }) => {
+<<<<<<< Updated upstream
 	const [users, setUsers] = useState([]);
+=======
+	const [users, setUsers] = useState([
+		{
+			username: "Victor",
+			userId: crypto.randomUUID(),
+		}
+	]);
+
+	const onUsernameChange = (e, userId) => {
+		const indexToUpdate = users.findIndex(user => user.userId === userId);
+		const tempUsers = [...users];
+		tempUsers[indexToUpdate] = {
+			...tempUsers[indexToUpdate],
+			username: e.target.value,
+		};
+		setUsers(tempUsers);
+	};
+
+>>>>>>> Stashed changes
 	const addUser = (username) => {
 		const newUser = {
 			username: username,
-			userId: crypto.randomUUID(),
-			count: 1
+			userId: crypto.randomUUID()
 		};
 		setUsers([
 			...users,
