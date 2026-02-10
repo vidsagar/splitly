66810f66d093e047b1f1865a8655d3f1b1f53b69
Victor <victor@Victors-MacBook-Pro.local>
2026-02-10 08:09:18 -0600

    style for inner nav

--- src/routes/SplitlyRoutes.jsx
+++ src/routes/SplitlyRoutes.jsx
@@ -4,12 +4,13 @@ import { Users } from "pages/splitly/Users";
 import { Activity } from "pages/splitly/Activity";
 import { Settings } from "pages/splitly/Settings";
 import { Link } from "react-router-dom";
+import "./Splitly.scss";
 
 function SplitlyRoutes() {
 	const page = (component) =>
-		<div className="splitly-nav-container">
+		<div className="splitly-container">
 			<main className="splitly-content">{component}</main>
-			<nav className="splitly-nav">
+			<nav className="splitly-nav-container">
 				<Link className="nav-item" to="/split/receipts">Receipts</Link>
 				<Link className="nav-item" to="/split/users">Users</Link>
 				<Link className="nav-item" to="/split/activity">Activity</Link>
